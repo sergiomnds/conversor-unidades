6f2dbe725a12f7c6e791f848d3ae6b695af0faaf
Luis Baiano <luisoft.77@gmail.com>
2024-12-14 06:40:15 -0300

    Bit_conversion Modified + Watts_conversion Added

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
+#include <stdio.h>
+
 #include "bit_conversion.h"
-#include "bit_conversion.c"
+#include "watts_conversion.h"
+
 
 int main() {
-    converteBits();
+    int conversao;
+
+    printf("Escolha a Conversão: \n"
+        "1 - Bits\n"
+        "2 - Watts\n"
+        "Opcao: ");
+    scanf("%d", &conversao);
+
+    switch (conversao) {
+        case 1:
+            converteBits();     // Chama a função de conversão de bits
+            break;
+        case 2:
+            converteWatts();    // Chama a função de conversão de watts
+            break;
+        default:
+            printf("Opção inválida. Encerrando o programa.\n");
+            break;
+    }
+
     return 0;
 }
